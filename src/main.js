3a32c5088c0565d18ade7d7fad09f44a864e595b
PeerHenry <peerhenry@gmail.com>
2018-12-09 23:07:26 +0100

    first slides

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Reveal from 'reveal.js'
+import infiLogo from './images/infi-logo.png'
+import webpackLogo from './images/webpack-logo.png'
+import webpackImage from './images/webpack.PNG'
 
-console.log('initializing reveal...')
 Reveal.initialize();
-console.log('initialized')
+
+const infiImg = document.getElementById('infi-logo');
+infiImg.src = infiLogo
+
+const webpackLogoImg = document.getElementById('webpack-logo');
+webpackLogoImg.src = webpackLogo
+
+const webpackImg = document.getElementById('webpack-image');
+webpackImg.src = webpackImage
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const buildDirName = 'dist'
+
 const input = {
   entry: path.resolve(__dirname, 'src/main.js')
 }
 
 const output = {
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, buildDirName),
     filename: 'bundle.js'
   }
 }
@@ -25,6 +27,12 @@ const loaders = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
+      {
+        test: /\.(png|PNG|jpg|jpeg|svg)$/,
+        use: [{
+          loader: 'file-loader', // could use url-loader for small files
+        }]
+      }
       // jsx
       // ts
       // pug
@@ -32,6 +40,9 @@ const loaders = {
       // scss
       // files: text, fonts, images
     ]
+  },
+  externals: {
+    // 'reveal.js': 'reveal.js',
   }
 }
 
@@ -44,7 +55,29 @@ const plugins = {
     new CopyWebpackPlugin([
       {
         from: 'node_modules/reveal.js/js/reveal.js',
-        to: 'dist/js/reveal.js'
+        to: 'js/reveal.js'
+      },
+      {
+        from: 'node_modules/reveal.js/css/reveal.css',
+        to: 'css/reveal.css'
+      },
+      {
+        from: 'node_modules/reveal.js/css/theme/*.css',
+        to: 'css/theme/[name].[ext]'
+      },
+      {
+        from: 'node_modules/reveal.js/css/print/*.css',
+        to: 'css/print/[name].[ext]'
+      },
+      {
+        context: 'node_modules/reveal.js/lib/',
+        from: '**/*',
+        to: 'lib/'
+      },
+      {
+        context: 'node_modules/reveal.js/plugin/',
+        from: '**/*',
+        to: 'plugin/'
       }
     ])
   ]
