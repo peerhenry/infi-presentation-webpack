59ec8faaecfdc9b6892564f117495e486a008b4e
PeerHenry <peerhenry@gmail.com>
2018-12-10 00:35:44 +0100

    verdere slides

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import infiLogo from './images/infi-logo.png'
 import webpackLogo from './images/webpack-logo.png'
 import webpackImage from './images/webpack.PNG'
 
-Reveal.initialize();
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+
 
 const infiImg = document.getElementById('infi-logo');
 infiImg.src = infiLogo
